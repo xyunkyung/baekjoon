987befda4d8161702b92771428a061ec9dbd74b1
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2020-08-17 21:26:10 +0900

    Add files via upload

---
+++ 10172.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#pragma warning (disable:4996)
+
+int main() {
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|\n");
+
+	getchar();
+	getchar();
+	return 0;
+}
